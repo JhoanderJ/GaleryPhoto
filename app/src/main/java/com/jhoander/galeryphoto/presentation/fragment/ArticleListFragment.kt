b1f68288fa0ab163a6b912a17f59c70c8613f128
jhoander <jhoander.torres@externos-cl.cencosud.com>
2021-01-07 19:23:21 -0300

    cardView to item of list

--- app/src/main/java/com/jhoander/galeryphoto/presentation/fragment/ArticleListFragment.kt
+++ app/src/main/java/com/jhoander/galeryphoto/presentation/fragment/ArticleListFragment.kt
@@ -44,7 +44,6 @@ class ArticleListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        articleRv.addItemDecoration(DividerItemDecoration(activity, LinearLayoutManager.VERTICAL))
         viewModel.getArticleList()
     }
 
